857d83fcadb5a46fe64c70fa72b673a440cafae6
jesusaldanamartin <jaldanam21@uma.es>
2022-11-28 11:38:07 +0100

    Link communities and cluster memberships
    * raynaud_genes.R updated with stringDB code
    
    * dudas.md questions to ask
    
    * report/figures new figures saved from the R code

--- code/raynaud_genes.R
+++ code/raynaud_genes.R
@@ -2,9 +2,11 @@ library(STRINGdb)
 library(igraph)
 library(linkcomm)
 
-nodes <- read.table("string_node_degrees.tsv", header = FALSE)
+string_db <- STRINGdb$new(version="11", species=9606, score_threshold=400, input_directory="")
 
-df <- read.table("string_interactions.tsv")
+nodes <- read.table("/home/jesusaldanamartin/Documentos/SistBio/raynaud/Gene_Phenotipycal_Study/code/string_node_degrees.tsv", header = FALSE)
+
+df <- read.table("/home/jesusaldanamartin/Documentos/SistBio/raynaud/Gene_Phenotipycal_Study/code/string_interactions.tsv")
 
 links <- data.frame(df$V3, df$V4)
 
@@ -13,6 +15,28 @@ links <- data.frame(df$V3, df$V4)
 
 g <- igraph::graph_from_data_frame(vertices = nodes$V2, d = links, directed = FALSE)
 
+g
+
+hits.network <- string_db$get_subnetwork(nodes$V2)
+
+first.neigh <- (neighbors(graph = g, v = V(hits.network)$name, mode = "all"))$name
+
+hits.network <- string_db$get_subnetwork(unique(c(V(hits.network)$name, first.neigh)))
+
+hits.df <- igraph::as_data_frame(hits.network, what="edges") 
+
+lc <- getLinkCommunities(hits.df, hcmethod = "average")
+
+plot(lc, type = "members")
+
+plot(
+  hits.network,
+  vertex.label = nodes$V1,
+  vertex.size = 10,
+  vertex.label.color = "black",
+  layout = layout.auto(g)
+)
+
 plot(
   g,
   vertex.label = nodes$V1,
@@ -20,3 +44,4 @@ plot(
   vertex.label.color = "black",
   layout = layout.auto(g)
      )
+
---
+++ dudas.md
@@ -0,0 +1,5 @@
+#Dudas para tutoria:
+
+¿Realizar estudio al nodo con mas conexiones al igual que con la comunidad?
+¿Que metodo de cluster jerarquizado utilizamos?¿single, average, complete?
+¿Como automatizar la descarga de datos desde stringDB?
