27abb82dde1992241b7481bb7858ad01a6ec831b
JCruiz15 <juancaruru@uma.es>
2022-11-29 17:38:46 +0100

    First steps into comunity analysis

--- code/raynaud_genes.R
+++ code/raynaud_genes.R
@@ -52,3 +52,56 @@ dev.off()
 png(file="../results/Raynaud_genes-comunities_graph.png", width=1080, height=1080)
 plot(lc, type = "graph", layout = layout.fruchterman.reingold, ewidth = 2, vlabel = FALSE)
 dev.off()
+
+
+
+################# Nested communities ##################
+
+# Nested communities: Son aquellas comunidades que son independientes respecto a otras
+getAllNestedComm(lc)
+# png("14_lc_nested_communities.png")
+plot(lc, type = "graph")
+# dev.off()
+
+################# Estudio funcionalidad ##################
+
+enriquecimiento <- function(cluster) {
+  # Extraemos sus datos
+  comunity_genes <- getNodesIn(lc, clusterids = cluster, type = "names")
+  expr_c <- genes_entrez[genes_entrez$STRING_id %in% comunity_genes, ]
+  
+  # Enriquecimiento funcional con GO
+  enrichmentGO <- string_db$get_enrichment(expr_c$STRING_id, category = "Process", iea = TRUE, )
+  enrichmentGO$ontology <- rep("GO")
+  
+  # Enriquecimiento funcional con KEGG
+  enrichmentKEGG <- string_db$get_enrichment(expr_c$STRING_id, category = "KEGG", iea = TRUE)
+  enrichmentKEGG$ontology <- rep("KEGG")
+  
+  # Enriquecimiento funcional con Pfam
+  enrichmentPfam <- string_db$get_enrichment(expr_c$STRING_id, category = "Pfam", iea = TRUE)
+  enrichmentPfam$ontology <- rep("Pfam")
+  
+  # Juntamos las dos
+  enrichment <- rbind(enrichmentGO, enrichmentKEGG, enrichmentPfam)
+  setcolorder(enrichment, c(11, c(1:10)))
+  
+  # Agrupamos filas repetidas indicando las ontolog?as de origen
+  for (i in enrichment$term[!duplicated(enrichment$term)]) {
+    num_filas <- which(enrichment$term == i)
+    ontologias <- paste(enrichment[num_filas, 1], collapse = ", ")
+    nueva_fila <- enrichment[num_filas[1],]
+    nueva_fila$ontology <- ontologias
+    enrichment <- enrichment[-num_filas, ]
+    enrichment <- rbind(enrichment, nueva_fila)
+  }
+  
+  return(enrichment)
+}
+
+for (i in c(1,2,3,4,5,6)) {
+  comunidad <- enriquecimiento(i)
+  View(comunidad)
+  dir <- paste("../results/Raynaud-Enriquecimiento_funcional-Comunidad_", i, ".csv", sep = "")
+  write.csv(comunidad, dir)
+}
