10be25e17faaa43f02baca36b762156f788a9fee
JCruiz15 <juancaruru@uma.es>
2022-11-29 17:11:54 +0100

    Automatic plot saving to results folder

--- code/raynaud_genes.R
+++ code/raynaud_genes.R
@@ -2,13 +2,17 @@ library(STRINGdb)
 library(igraph)
 library(linkcomm)
 
-string_db <- STRINGdb$new(version="11", species=9606)
+wd <- readline("Insert the code directory: ")
+setwd(wd)
+
+string_db <- STRINGdb$new(version="11", species=9606, score_threshold=700)
 
 string.network <- string_db$get_graph()
 
 genes <- read.csv("genes_for_HP_0030880.csv", sep=";")
 
 genes_entrez <- string_db$map(my_data_frame = genes, my_data_frame_id_col_names = "GENE_SYMBOL")
+
 
 # g <- igraph::graph_from_data_frame(vertices = nodes$V2, d = links, directed = FALSE)
 
@@ -22,10 +26,11 @@ hits.network <- string_db$get_subnetwork(unique(c(V(hits.network)$name, first.ne
 
 hits.df <- igraph::as_data_frame(hits.network, what="edges") 
 
-lc <- getLinkCommunities(hits.df, hcmethod = "average")
+lc <- linkcomm::getLinkCommunities(hits.df, hcmethod = "average")
 
-plot(lc, type = "members")
+# Saving plots
 
+png(file="../results/Raynaud_genes-graph.png", width=850, height=850)
 plot(
   hits.network,
   vertex.label = genes_entrez$GENE_SYMBOL,
@@ -33,6 +38,17 @@ plot(
   vertex.label.color = "black",
   layout = layout.auto(hits.network)
 )
+dev.off()
 
-plot(hits.network_lc, type = "graph", layout = layout.fruchterman.reingold, ewidth = 2, vlabel = FALSE)
+png(file="../results/Raynaud_genes-dendrogram.png", width=500, height=500)
+plot(lc, type = "summary")
+dev.off()
 
+
+png(file="../results/Raynaud_genes-comunity_members_matrix.png", width=500, height=500)
+plot(lc, type = "members")
+dev.off()
+
+png(file="../results/Raynaud_genes-comunities_graph.png", width=1080, height=1080)
+plot(lc, type = "graph", layout = layout.fruchterman.reingold, ewidth = 2, vlabel = FALSE)
+dev.off()
