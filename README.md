ffefe61de4ec9c4261dd85d6de5cf410f0b8ad3e
JCruiz15 <juancaruru@uma.es>
2022-11-30 14:10:04 +0100

    Results section + bibliography

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Gene_Phenotipycal_Study
+# Gene_Phenotypical_Study
 
 Systems Biology project from Málaga University, Health Engineering (2022). This project is the result of the final task for Systems Biology subject. It contains information about the gene HP:0030880, gene related to the rare disease Raynaud phenomenon. Authors: Jesús Aldana Martín & Juan Carlos Ruiz Ruiz
--- code/raynaud_genes.R
+++ code/raynaud_genes.R
@@ -1,9 +1,10 @@
 library(STRINGdb)
 library(igraph)
 library(linkcomm)
+library(data.table)
 
-wd <- readline("Insert the code directory: ")
-setwd(wd)
+# wd <- readline("Insert the code directory: ")
+# setwd(wd)
 
 string_db <- STRINGdb$new(version="11", species=9606, score_threshold=700)
 
@@ -12,9 +13,6 @@ string.network <- string_db$get_graph()
 genes <- read.csv("genes_for_HP_0030880.csv", sep=";")
 
 genes_entrez <- string_db$map(my_data_frame = genes, my_data_frame_id_col_names = "GENE_SYMBOL")
-
-
-# g <- igraph::graph_from_data_frame(vertices = nodes$V2, d = links, directed = FALSE)
 
 hits.network <- string_db$get_subnetwork(genes_entrez$STRING_id)
 
@@ -34,7 +32,13 @@ png(file="../results/Raynaud_genes-graph.png", width=850, height=850)
 plot(
   hits.network,
   vertex.label = genes_entrez$GENE_SYMBOL,
+  vertex.frame.color = NA,
   vertex.size = 10,
+  edge.weight = edge.betweenness(hits.network),
+  edge.color = "grey",
+  reescale=TRUE,
+  vertex.label.dist = 1.8,
+  vertex.label.cex = 1,
   vertex.label.color = "black",
   layout = layout.auto(hits.network)
 )
@@ -58,15 +62,15 @@ dev.off()
 
 # Nested communities: Son aquellas comunidades que son independientes respecto a otras
 getAllNestedComm(lc)
-# png("14_lc_nested_communities.png")
+png("../results/Raynaud-nested_communities.png")
 plot(lc, type = "graph")
-# dev.off()
+dev.off()
 
 ################# Estudio funcionalidad ##################
 
 enriquecimiento <- function(cluster) {
   # Extraemos sus datos
-  comunity_genes <- getNodesIn(lc, clusterids = i, type = "names")
+  comunity_genes <- getNodesIn(lc, clusterids = cluster, type = "names")
   expr_c <- genes_entrez[genes_entrez$STRING_id %in% comunity_genes, ]
   
   # Enriquecimiento funcional con GO
@@ -74,33 +78,32 @@ enriquecimiento <- function(cluster) {
   enrichmentGO$ontology <- rep("GO")
   
   # Enriquecimiento funcional con KEGG
-  enrichmentKEGG <- string_db$get_enrichment(expr_c$STRING_id, category = "KEGG", iea = TRUE)
-  enrichmentKEGG$ontology <- rep("KEGG")
+  # enrichmentKEGG <- string_db$get_enrichment(expr_c$STRING_id, category = "KEGG", iea = TRUE)
+  # enrichmentKEGG$ontology <- rep("KEGG")
   
   # Enriquecimiento funcional con Pfam
-  #enrichmentPfam <- string_db$get_enrichment(expr_c$STRING_id, category = "Pfam", iea = TRUE)
-  #enrichmentPfam$ontology <- rep("Pfam")
+  # enrichmentPfam <- string_db$get_enrichment(expr_c$STRING_id, category = "Pfam", iea = TRUE)
+  # enrichmentPfam$ontology <- rep("Pfam")
   
   # Juntamos las dos
-  enrichment <- rbind(enrichmentGO, enrichmentKEGG)
+  enrichment <- rbind(enrichmentGO)
   data.table::setcolorder(enrichment, c(11, c(1:10)))
-
-  # Agrupamos filas repetidas indicando las ontolog?as de origen
-  for (i in enrichment$term[!duplicated(enrichment$term)]) {
-    num_filas <- which(enrichment$term == i)
-    ontologias <- paste(enrichment[num_filas, 1], collapse = ", ")
-    nueva_fila <- enrichment[num_filas[1],]
-    nueva_fila$ontology <- ontologias
-    enrichment <- enrichment[-num_filas, ]
-    enrichment <- rbind(enrichment, nueva_fila)
-  }
-  enrichment
+  
   return(enrichment)
 }
 
-for (i in c(1,2,3,4,5,6)) {
-  comunidad <- enriquecimiento(i)
-  View(comunidad)
-  dir <- paste("../results/Raynaud_Enriquecimiento_funcional_Comunidades", i, ".csv", sep = "")
-  write.csv(comunidad, dir)
+for (i in c(1:6)) {
+  tryCatch(
+    {
+      comunidad <- enriquecimiento(i)
+      dir <- paste("../results/Raynaud_Enriquecimiento_funcional_Comunidades/Comunity", i, ".csv", sep = "")
+      write.csv(comunidad, dir)
+    },
+    error=function(err) {
+      message(paste("Error in comunity", i, ":", sep = " "))
+      message(err)
+      message("\n")
+      return(NA)
+    }
+  )
 }
